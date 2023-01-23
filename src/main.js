629d450f9948e4c1e142e62defe00c35c77fd0e1
AndreaFlo1794 <andreaflo1794@gmail.com>
2023-01-23 17:27:44 -0500

    display

--- src/main.js
+++ src/main.js
@@ -1,6 +1,42 @@
 import data from './data/ghibli/ghibli.js'; //importar datos
 import { orderingBy } from './data.js';
 
+
+//display para las interfaces
+const navHome= document.querySelector("#nav-home");
+const navFilms= document.querySelector("#nav-films");
+const navCharacters= document.querySelector("#nav-characters");
+
+const secHome=document.querySelector("#home");
+const secFilms=document.querySelector("#films");
+const secCharacters=document.querySelector("#characters");
+
+navHome.addEventListener("click", ()=>{
+  secHome.style.display="block";
+  secFilms.style.display="none";
+  secCharacters.style.display="none";
+})
+
+navFilms.addEventListener("click", ()=>{
+
+  secHome.style.display="none";
+  secFilms.style.display="block";
+  secCharacters.style.display="none";
+
+})
+
+navCharacters.addEventListener("click", ()=>{
+  secHome.style.display="none";
+  secFilms.style.display="none";
+  secCharacters.style.display="block";
+})
+
+
+
+
+
+
+
 //variable donde almacenamos los datos para trabajar más comodamente
 const getData = data.films
 
@@ -12,8 +48,7 @@ function drawFilms(filmsArray){               //Funcion cuyo unico fin es PINTAR
 
      return  `
     <div class="film-container" id= " ${film.id} ">
-      
-      <h4 class="title">${film.title} </h4> 
+     
       <img src="${film.poster}">
       <h4> 📆 ${film.release_date} </h4>
       <h5> ⭐ ${film.rt_score} </h5>
@@ -28,16 +63,11 @@ function drawFilms(filmsArray){               //Funcion cuyo unico fin es PINTAR
   
   console.log(containerPicture.querySelectorAll('.film-container'))
   return containerPicture
-  
-
-
 }
 
 
 //Mostrando datos en la interfaz
  drawFilms(getData) 
-
-
 
 //ordenando por medio de un select
 const drawRating = document.getElementById('desplegar-orden')
@@ -53,6 +83,9 @@ drawRating.addEventListener("change" , () =>{
   if(drawRating.value === ""){dataContainerEl.innerHTML = drawFilms(getData)}
   
 })
+
+
+
 
 
 
