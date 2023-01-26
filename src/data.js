80c564f0a1c6cfe3b0ee1b8552812b43b490c609
isabelroman84 <isabelcristinaroman@gmail.com>
2023-01-25 19:00:22 -0500

    filter function

--- src/data.js
+++ src/data.js
@@ -16,14 +16,6 @@ export const filteringBy = (data , key)=>{
   })
  
 }
-    //  const result= /Hayao/i.test(film[key])
-    //  const result2= /Isao/i.test(film[key])
-    //  return result || result2
-    // const result = /female/i.test(film[key]) || /Male/i.test(film[key])
-    // // const result2 = 
-  //   console.log(result)
-  //    return result 
-  // })
 
 
 
